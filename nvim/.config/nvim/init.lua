3fafd4f6422e9cfcc04e77f943d9b46c04192b40
onon1101 <mingzhuguly@gmail.com>
2024-02-12 11:44:58 +0800

    change transparent background

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,11 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
-polish = function()
-require("notify").setup({
-  background_colour = "#1a1b26",
-})
-
 vim.schedule(function()
   vim.opt.termguicolors = true
 end)
